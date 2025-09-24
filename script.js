19e401b28bb3c95ba62501bb90964796c8b7d553
Metzeler Tumamak <208787873+Metzeler-Tumamak@users.noreply.github.com>
2025-09-24 13:15:58 +0800

    Add basic arithmetic operations

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
 const display = document.querySelector("input");
 const container = document.querySelector("#container");
+let total = 0;
+let clearDisplay = false;
+let lastOperation = null;
+let lastOperand = null;
 
 function appendCharacter(char) {
+  if (clearDisplay) {
+    display.value = "";
+    clearDisplay = false;
+  }
   displayValue = display.value;
   display.value = displayValue.startsWith("0") ? char : displayValue + char;
 }
 
-const leftHandOperand = 0;
-const rightHandOperand = null;
-// const numbers = document.querySelectorAll(".number");
+function processArithmetic(operator, rightHandOperand = null) {
+  if (operator === "=") {
+    processArithmetic(lastOperation, lastOperand);
+    return;
+  }
+
+  // let displayVal = 0;
+  // if (operator !== lastOperation && lastOperation !== null) {
+  //   displayVal = 0;
+  // } else {
+  //   displayVal = rightHandOperand ?? Number(display.value);
+  // }
+
+  if (lastOperation === null) {
+    lastOperation = operator;
+  }
+
+  const displayVal = rightHandOperand ?? Number(display.value);
+
+  switch (lastOperation) {
+    case "+":
+      total += displayVal;
+      break;
+    case "-":
+      total -= displayVal;
+      break;
+    case "*":
+      total *= displayVal;
+      break;
+    case "/":
+      total /= displayVal;
+      break;
+  }
+  lastOperand = displayVal;
+  lastOperation = operator;
+  display.value = total.toString();
+  clearDisplay = true;
+}
 
 container.addEventListener("click", (event) => {
   const btnRef = event.target;
-  //   const buttonTypes = ["number", "arithmetic", "fn"];
   const buttonTypes = {
     number: appendCharacter,
-    arithmetic: null,
+    arithmetic: processArithmetic,
     fn: null,
   };
 
